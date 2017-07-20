f0c3c2b1687547744329995371c1770bcc2ddd58
Ch4rlieB <charlieb@centrum.cz>
2017-07-20 18:08:19 +0200

    Přidání scriptu camera.py, který po spuštění bude každých 10 minut vytvářet fotku

---
+++ camera.py
@@ -0,0 +1,26 @@
+import picamera
+import time
+import os
+
+# get instance of PiCamera
+camera = picamera.PiCamera()
+
+# camera settings for resolution, horizontal flip, vertical flip
+camera.resolution = '1080p'
+camera.hflip = True
+camera.vflip = True
+
+time.sleep(1*60)
+
+# infinite loop
+while True:
+
+  # picture path settings
+  capture_time = time.strftime("%Y-%m-%d-%H%M%S")
+  picture_path = '/var/www/html/pics/image' + capture_time + '.jpg'
+
+  # capturing picture
+  camera.capture(picturepath)
+
+  # sleep 10 minutes before capturing next picture
+  time.sleep(10*60)
